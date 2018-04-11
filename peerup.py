de506c170acf55bf8e09bc4a0d501b3c73065014
Taiji Tsuchiya <tuty0630@gmail.com>
2018-04-10 20:15:23 -0700

    create function: display Operation info, Host info

--- peerup.py
+++ peerup.py
@@ -10,9 +10,24 @@ def load_scenario_file(scenario_filename):
     return param
 
 
-
 if __name__ == '__main__':
+
     scenario_filename = 'scenarios/scenario_demo_vsrx.yml'
 
     param = load_scenario_file(scenario_filename)
-    pprint(param)
+    #pprint(param)
+
+    print('===== Operation Information =====')
+    print('purpus:')
+    print(param['purpus'])
+    print('Operator        : ' + param['operator'])
+    print('Operation Date  : ' + str(param['operation_date']))
+
+    print('===== Host Information =====')
+    print('Host Name        : ' + param['hosts']['hostname'])
+    print('os               : ' + param['hosts']['os'])
+    print('User Name        : ' + param['hosts']['username'])
+    print('Password         : ' + param['hosts']['password'])
+    print('mgmt_ipaddress   : ' + param['hosts']['mgmt_ipaddress'])
+
+
