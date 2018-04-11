117714f153d83dfc2104e5ddc5b56175187a6e84
Taiji Tsuchiya <tuty0630@gmail.com>
2018-04-10 22:24:38 -0700

    complemte to add check_hostname

--- peerup.py
+++ peerup.py
@@ -20,73 +20,90 @@ def load_scenario_file(scenario_filename):
     return param
 
 
-def check_hostname(param):
-    print('Check Hostname', end='')
-    print(Fore.GREEN + 'OK')
+def check_hostname(device, param):
+    print('Check Hostname : ', end='')
+
+    hostname_expected   = param['hostname']
+    hostname_actual     = device.get_facts()['hostname']
+
+    if hostname_actual == hostname_expected:
+        print(Fore.GREEN + 'OK')
+        print(Fore.GREEN + 'expected : ' +  hostname_expected)
+        print(Fore.GREEN + 'actual   : ' +  hostname_actual)
+    else:
+        print(Fore.RED + 'NG')
+        print(Fore.RED + 'expected : ' +  hostname_expected)
+        print(Fore.RED + 'actual   : ' +  hostname_actual)
 
 
-def check_model(param):
+def check_model(device, param):
     print('Check Model : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def check_os_version(param):
+def check_os_version(device, param):
     print('Check OS Version : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def check_interface(param):
+def check_interface(device, param):
     print('Check Interface : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def check_bgp_neighbor(param):
+def check_bgp_neighbor(device, param):
     print('Check BGP Neighbor : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def check_bgp_route_received(param):
+def check_bgp_route_received(device, param):
     print('Check BGP Roue Received : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def check_bgp_route_advertised(param):
+def check_bgp_route_advertised(device, param):
     print('Check BGP Roue Adcertised : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def set_interface(param):
+def set_interface(device, param):
     print('Set Interface : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def set_bgp_neighbor(param):
+def set_bgp_neighbor(device, param):
     print('Set BGP Neighbor : ', end='')
     print(Fore.GREEN + 'OK')
 
 
 
-def set_bgp_route_received(param):
+def set_bgp_route_received(device, param):
     print('Set BGP Route Recieved : ', end='')
     print(Fore.GREEN + 'OK')
 
 
-def set_bgp_route_advertised(param):
+def set_bgp_route_advertised(device, param):
     print('Set BGP Route Advertised : ', end='')
     print(Fore.GREEN + 'OK')
 
 
-
-def exec_scenario(operation_list):
+def exec_scenario(device, operation_list):
     for operation in operation_list:
-        print(operation)
+        opr_name  = next(iter(operation))
+        opr_param = operation[opr_name]
+        #print(opr_name)
+
+        if opr_name == 'check_hostname':
+            check_hostname(device, opr_param)
+        elif opr_name == 'check_model':
+            check_model(device, opr_param)
 
 
 if __name__ == '__main__':
@@ -114,16 +131,14 @@ if __name__ == '__main__':
 
     print('===== Login Router =====')
     print("login router : ", end='')
-    
     driver = napalm.get_network_driver(param['hosts']['os'])
     device = driver(
                 hostname=param['hosts']['mgmt_ipaddress'],
                 username=param['hosts']['username'],
                 password=param['hosts']['password'])
     device.open()
-    
     print(Fore.GREEN + 'OK')
 
 
     print('===== Run Scenario =====')
-    #exec_scenario(param['scenario'])
+    exec_scenario(device, param['scenario'])
