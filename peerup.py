84de006ece40b2044c87ac7de3d3a642a4255ff8
Taiji Tsuchiya <tuty0630@gmail.com>
2018-04-10 21:04:33 -0700

    add scenario func as empty

--- peerup.py
+++ peerup.py
@@ -1,5 +1,15 @@
 import yaml
+import napalm
+
+# For Color Font
+from colorama import init as colorama_init
+from colorama import Fore
+
 from pprint import pprint
+
+# Init Color Font
+colorama_init(autoreset=True)
+
 
 def load_scenario_file(scenario_filename):
     with open(scenario_filename, 'r') as file:
@@ -10,6 +20,58 @@ def load_scenario_file(scenario_filename):
     return param
 
 
+def check_hostname(param):
+    pass
+
+
+def check_model(param):
+    pass
+
+
+def check_os_version(param):
+    pass
+
+
+def check_interface(param):
+    pass
+
+
+def check_bgp_neighbor(param):
+    pass
+
+
+def check_bgp_route_received(param):
+    pass
+
+
+def check_bgp_route_advertised(param):
+    pass
+
+
+def set_interface(param):
+    pass
+
+
+def set_bgp_neighbor(param):
+    pass
+
+
+def set_bgp_route_received(param):
+    pass 
+
+
+def set_bgp_route_advertised(param):
+    pass
+
+
+
+def exec_scenario(operation_list):
+    for operation in operation_list:
+        print(operation)
+
+
+
+
 if __name__ == '__main__':
 
     scenario_filename = 'scenarios/scenario_demo_vsrx.yml'
@@ -17,11 +79,13 @@ if __name__ == '__main__':
     param = load_scenario_file(scenario_filename)
     #pprint(param)
 
+
     print('===== Operation Information =====')
     print('purpus:')
     print(param['purpus'])
     print('Operator        : ' + param['operator'])
     print('Operation Date  : ' + str(param['operation_date']))
+
 
     print('===== Host Information =====')
     print('Host Name        : ' + param['hosts']['hostname'])
@@ -31,3 +95,18 @@ if __name__ == '__main__':
     print('mgmt_ipaddress   : ' + param['hosts']['mgmt_ipaddress'])
 
 
+    print('===== Login Router =====')
+    print("login router : ", end="")
+    '''
+    driver = napalm.get_network_driver(param['hosts']['os'])
+    device = driver(
+                hostname=param['hosts']['mgmt_ipaddress'],
+                username=param['username'],
+                password=param['password'])
+    device.open()
+    '''
+    print(Fore.GREEN + "OK")
+
+
+    print('===== Run Scenario =====')
+    exec_scenario(param['scenario'])
